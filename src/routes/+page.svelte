24f47e950995021dac4033c78787a98f730eec1b
Tobias <19623985+Sidewinder53@users.noreply.github.com>
2023-05-03 20:56:33 +0000

    feat: Basic calc functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,87 @@
 <script>
   import 'carbon-components-svelte/css/g10.css';
   import {
-    Header,
-    Content,
-    Grid,
-    Row,
-    Column,
-    NumberInput,
-    DatePicker,
-    DatePickerInput,
     Accordion,
     AccordionItem,
-    LocalStorage,
-    SkipToContent,
+    Column,
+    Content,
+    DatePicker,
+    DatePickerInput,
+    Dropdown,
+    Grid,
+    Header,
     HeaderNav,
     HeaderNavItem,
-    HeaderNavMenu
+    HeaderNavMenu,
+    LocalStorage,
+    NumberInput,
+    Row,
+    SkipToContent
   } from 'carbon-components-svelte';
+
+  import {
+    differenceInMonths,
+    getDate,
+    getDaysInMonth,
+    lastDayOfMonth,
+    parse,
+    setDate,
+    subMonths
+  } from 'date-fns';
+
+  let carClasses = [
+    {
+      name: 'upTo250',
+      title: 'Up to 250 metric horse power 🚙',
+      surchargeLower: 0.19,
+      surchargeHigher: 0.29,
+      limit: 4000
+    },
+    {
+      name: 'moreThan250',
+      title: 'More than 250 metric horse power 🏎️',
+      surchargeLower: 0.29,
+      surchargeHigher: 0.39,
+      limit: 4000
+    }
+  ];
 
   let currentKm = 0;
   let initialKm = 0;
-  let monthlyKmAllowance = 0;
-  /**
-   * @type {Date}
-   */
-  let checkOutDate;
-  /**
-   * @type {Date}
-   */
-  let checkInDate;
+  let allowanceMonthly = 0;
+  let checkOutValue = '';
+  let checkInValue = '';
+  let carClass = 0;
+
+  $: checkOutDate = parse(checkOutValue, 'dd/MM/yyyy', new Date());
+  $: checkInDate = parse(checkInValue, 'dd/MM/yyyy', new Date());
+
+  // Calculations
+  $: allowanceFactorMonthCO = 1 - (getDate(checkOutDate) - 1) / getDaysInMonth(checkOutDate);
+  $: allowanceFactorMonthCI = getDate(checkInDate) / getDaysInMonth(checkInDate);
+  $: allowanceMonthCO = allowanceMonthly * allowanceFactorMonthCO;
+  $: allowanceMonthCI = allowanceMonthly * allowanceFactorMonthCI;
+
+  // Calculate number of full months by skipping the CO and CI months entirely
+  $: fullMonths = differenceInMonths(
+    setDate(subMonths(checkInDate, 1), 1),
+    lastDayOfMonth(subMonths(checkOutDate, 1))
+  );
+
+  $: allowanceTotal = allowanceMonthly * fullMonths + allowanceMonthCO + allowanceMonthCI;
 </script>
+
+{@debug allowanceFactorMonthCI}
 
 <LocalStorage key="currentKm" bind:value={currentKm} />
 <LocalStorage key="initialKm" bind:value={initialKm} />
-<LocalStorage key="monthlyKmAllowance" bind:value={monthlyKmAllowance} />
-<LocalStorage key="checkOutDate" bind:value={checkOutDate} />
-<LocalStorage key="checkInDate" bind:value={checkInDate} />
+<LocalStorage key="monthlyKmAllowance" bind:value={allowanceMonthly} />
+<LocalStorage key="checkOutDate" bind:value={checkOutValue} />
+<LocalStorage key="checkInDate" bind:value={checkInValue} />
+<LocalStorage key="carClass" bind:value={carClass} />
 
 <Header platformName="Fleetwächter" expandedByDefault={false} expansionBreakpoint={320}>
-  <svelte:fragment slot="skip-to-content">
+  <!-- <svelte:fragment slot="skip-to-content">
     <SkipToContent />
   </svelte:fragment>
   <HeaderNav>
@@ -50,7 +93,7 @@
       <HeaderNavItem href="/" text="Link 2" />
       <HeaderNavItem href="/" text="Link 3" />
     </HeaderNavMenu>
-  </HeaderNav>
+  </HeaderNav> -->
 </Header>
 
 <Content>
@@ -59,23 +102,27 @@
       <Column>
         <NumberInput hideSteppers label="Current kilometers" bind:value={currentKm} />
         <Accordion>
-          <AccordionItem title="Configure kilometer allowance">
+          <AccordionItem title="Configure kilometer allowance" open={true}>
             <Grid>
               <Row>
                 <Column>
-                  <DatePicker datePickerType="single" dateFormat="d/m/Y" on:change>
-                    <DatePickerInput
-                      labelText="Check out date (when you got the car)"
-                      bind:value={checkOutDate}
-                    />
+                  <DatePicker
+                    datePickerType="single"
+                    dateFormat="d/m/Y"
+                    on:change
+                    bind:value={checkOutValue}
+                  >
+                    <DatePickerInput labelText="Check out date (when you got the car)" />
                   </DatePicker>
                 </Column>
                 <Column>
-                  <DatePicker datePickerType="single" dateFormat="d/m/Y" on:change>
-                    <DatePickerInput
-                      labelText="Check in date (when you return the car)"
-                      bind:value={checkInDate}
-                    />
+                  <DatePicker
+                    datePickerType="single"
+                    dateFormat="d/m/Y"
+                    on:change
+                    bind:value={checkInValue}
+                  >
+                    <DatePickerInput labelText="Check in date (when you return the car)" />
                   </DatePicker>
                 </Column>
               </Row>
@@ -84,7 +131,7 @@
                   <NumberInput
                     hideSteppers
                     label="Monthly kilometer allowance"
-                    bind:value={monthlyKmAllowance}
+                    bind:value={allowanceMonthly}
                   />
                 </Column>
                 <Column>
@@ -95,10 +142,57 @@
                   />
                 </Column>
               </Row>
+              <Row>
+                <Column>
+                  <Dropdown
+                    titleText="Car class"
+                    bind:selectedId={carClass}
+                    items={carClasses.map((carClass, index) => ({
+                      id: index,
+                      text: carClass.title
+                    }))}
+                    let:item
+                    let:index
+                  >
+                    <div>
+                      <strong>{carClasses[index].title}</strong>
+                    </div>
+                    <div>
+                      Lower: {carClasses[index].surchargeLower}€ - Higher: {carClasses[index]
+                        .surchargeHigher}€ - Limit: {carClasses[index].limit} km
+                    </div>
+                  </Dropdown>
+                </Column>
+              </Row>
             </Grid>
           </AccordionItem>
         </Accordion>
       </Column>
     </Row>
+    <br />
+    <Row>
+      <h3>Debug information</h3>
+    </Row>
+    <Row>
+      This information may be used to debug calculations.
+      <br /><br />
+    </Row>
+    <Row>
+      <p>
+        CO Date: {checkOutDate} - applied factor: {allowanceFactorMonthCO} (amounts to {allowanceMonthCO}
+        km)<br />
+        CI Date: {checkInDate} - applied factor: {allowanceFactorMonthCI} (amounts to {allowanceMonthCI}
+        km)<br />
+        Months with full allowance: {fullMonths} (amounts to {allowanceMonthly * fullMonths} km)<br
+        />
+        Total allowance: {allowanceTotal} km
+      </p>
+    </Row>
   </Grid>
 </Content>
+
+<style>
+  :global(.bx--list-box__menu-item, .bx--list-box__menu-item__option) {
+    height: auto;
+  }
+</style>
